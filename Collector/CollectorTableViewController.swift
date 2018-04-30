31e9dc47c2e9019067c923da4c8acac136dd46a8
Ivaylo Yosifov <yosifov@gmail.com>
2018-05-01 02:23:01 +0300

    Final version

--- Collector/CollectorTableViewController.swift
+++ Collector/CollectorTableViewController.swift
@@ -46,10 +46,26 @@ class CollectorTableViewController: UITableViewController {
 
         let item = items[indexPath.row]
         cell.textLabel?.text = item.title
-        
+        if let imageData = item.image {
+            cell.imageView?.image = UIImage(data: imageData)
+        }
 
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let item = items[indexPath.row]
+                context.delete(item)
+                getItems()
+            }
+        }
+    }
 
 }
